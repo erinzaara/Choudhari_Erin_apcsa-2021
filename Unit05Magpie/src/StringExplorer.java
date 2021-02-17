1f6c8fa50c66c2d1e0cacda532d0a75d23eabab6
erinzaara <67299474+erinzaara@users.noreply.github.com>
2021-02-16 20:47:43 -0800

    Unit05magpie

---
+++ Unit05Magpie/src/StringExplorer.java
@@ -0,0 +1,33 @@
+/**
+ * A program to allow students to try out different 
+ * String methods. 
+ * @author Laurie White
+ * @version April 2012
+ */
+public class StringExplorer
+{
+
+	public static void main(String[] args)
+	{
+		String sample = "The quick brown fox jumped over the lazy dog.";
+		
+		//  Demonstrate the indexOf method.
+		int position = sample.indexOf("quick");
+		System.out.println ("sample.indexOf(\"quick\") = " + position);
+		
+		//  Demonstrate the toLowerCase method.
+		String lowerCase = sample.toLowerCase();
+		System.out.println ("sample.toLowerCase() = " + lowerCase);
+		System.out.println ("After toLowerCase(), sample = " + sample);
+		
+		//  Try other methods here:
+		int pos = sample.indexOf("slow");
+		if(pos != -1)
+		   System.out.println("slow is found at position " + pos);
+		else
+		   System.out.println("slow is not found");
+
+
+	}
+
+}
